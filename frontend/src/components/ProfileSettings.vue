97798e4d4d75c402d94f48f7ccfa053d7456f625
Nora <norajr@storroe.no>
2025-05-16 08:09:37 +0200

    able to update the username now Changes to be committed: 	modified:   backend/controllers/auth.controller.js 	modified:   backend/routes/auth.routes.js 	modified:   frontend/src/components/ProfileSettings.vue 	modified:   frontend/src/store/authStore.js

--- frontend/src/components/ProfileSettings.vue
+++ frontend/src/components/ProfileSettings.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="profile-settings">
     <h1>Profile Information</h1>
+
     <div v-if="user">
-      <img class="avatar" :src="user.avatar" alt="User Avatar">
+      <img class="avatar" :src="user.avatar" alt="User Avatar" />
       <p><strong>Email:</strong> {{ user.email }}</p>
-      <p><strong>Username:</strong> {{ user.username }}</p>
+
+      <div v-if="editing">
+        <input v-model="form.username" />
+        <button @click="saveUsername">Save</button>
+        <button @click="cancelEdit">Cancel</button>
+      </div>
+      <div v-else>
+        <p><strong>Username:</strong> {{ user.username }}</p>
+        <button @click="startEdit">Edit Username</button>
+      </div>
     </div>
+
     <div v-else>
       <p>Loading user information...</p>
     </div>
@@ -14,13 +25,48 @@
 
 <script>
 import { useAuthStore } from "@/store/authStore";
+import api from "@/api/axios"; // Make sure this is set up to point to your backend
 
 export default {
   name: "ProfileSettings",
+  data() {
+    return {
+      editing: false,
+      form: {
+        username: "",
+      },
+    };
+  },
   computed: {
     user() {
-      const authStore = useAuthStore();
-      return authStore.user;
+      return useAuthStore().user;
+    },
+  },
+  methods: {
+    startEdit() {
+      this.editing = true;
+      this.form.username = this.user.username;
+    },
+    cancelEdit() {
+      this.editing = false;
+      this.form.username = this.user.username;
+    },
+    async saveUsername() {
+      try {
+        const response = await api.patch("/auth/me", {
+          username: this.form.username,
+        });
+
+        // Update user in store
+        const authStore = useAuthStore();
+        authStore.setUser(response.data.researcher); // Assuming your PATCH route returns updated researcher
+
+        this.editing = false;
+        alert("Username updated!");
+      } catch (error) {
+        console.error(error);
+        alert("Failed to update username.");
+      }
     },
   },
 };
@@ -37,9 +83,16 @@ export default {
   padding: 20px;
   text-align: center;
 }
-
 p {
   font-size: 1.5rem;
   margin: 10px 0;
+}
+input {
+  font-size: 1.2rem;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+button {
+  margin: 0 5px;
 }
 </style>
