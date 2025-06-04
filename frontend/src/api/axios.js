4133b87cd2888579f32f7c71a01e13785edd6b1a
Hanaka15 <hanakakiller@gmail.com>
2025-06-04 17:59:24 +0200

    3rd time is whatever...

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://group2.sustainability.it.ntnu.no/api',
   withCredentials: true,
 });
 
